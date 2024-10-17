d108d8989d489312ac274094ebf045d07cd4ce02
boxu555 <bo.xu@estudiante.epsum.school>
2024-10-17 11:44:37 +0200

    Corregidos por segunda vez

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
 	public static void main (String[]args){
 	for (int i = 0 ;i < args.length; i++) {
-		System.out.println("Argumentos" + i + ":" + args[i]);	
-		}
+	try {	
+	System.out.println("Argumentos" + i + ":" + args[i]);	
+	} catch (ArrayIndexOutOfBoundsException e){
+		System.out.println("error" + e.getMessage());
+}
 	}
+}
 }
