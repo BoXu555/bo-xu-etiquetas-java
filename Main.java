95dcfd38e561e8c99d6bad2f14e034936db7a455
boxu555 <bo.xu@estudiante.epsum.school>
2024-10-17 11:03:01 +0200

    Bucle for creado

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 public class Main {
 	public static void main (String[]args){
-		System.out.println(args.length);
-		if (args.length == 0) {
-			System.out.println("No hay argumentos");
-		} else {
-			System.out.println("Hay" + args.length() + "argumentos");
+	for (int i = 0, args.length() < i, i++) {
+		System.out.println("Argumentos" + i + ":" args[i]);	
 		}
 	}
 }
